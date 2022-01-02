c250f560539eacf180a430ea8911978fcba95597
chiupam <chiupam@126.com>
2022-01-02 19:33:21 +0800

    增加监听群组记录的key

--- beta/bot/update.py
+++ beta/bot/update.py
@@ -1,4 +1,6 @@
-version = 'version :1.0.0'
+version = 'version :1.0.1 beta'
 botlog = '''
 **2022年1月2日**
+整合SuMaiKaDe大佬的[bot](https://github.com/SuMaiKaDe/bot.git)仓库，直接部署自定义机器人即可。
+可用的指令通过/start或者/help进行查看。
 '''
