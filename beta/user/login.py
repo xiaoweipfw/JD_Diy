310c6a948468e4adc4b661d64bb4e8324386fc0a
chiupam <chiupam@126.com>
2022-01-02 12:34:17 +0800

    修复/user指令

--- beta/user/login.py
+++ beta/user/login.py
@@ -19,7 +19,6 @@ from telethon import events, Button
 from .. import chat_id, jdbot, logger, TOKEN
 from ..bot.utils import press_event, V4, CONFIG_SH_FILE, row, split_list, AUTH_FILE, get_cks
 
-
 if BOT.get('proxy_user') and BOT['proxy_user'] != "代理的username,有则填写，无则不用动":
     proxy = {
         'proxy_type': BOT['proxy_type'],
@@ -40,32 +39,42 @@ elif BOT.get('noretry') and BOT['noretry']:
     user = TelegramClient(f'{CONFIG_DIR}/user', API_ID, API_HASH)
 else:
     user = TelegramClient(f'{CONFIG_DIR}/user', API_ID, API_HASH, connection_retries=None)
-    
-    
+
+
 def restart():
     text = "if [ -d '/jd' ]; then cd /jd/jbot; pm2 start ecosystem.config.js; cd /jd; pm2 restart jbot; else " \
-            "ps -ef | grep 'python3 -m jbot' | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null; " \
-            "nohup python3 -m jbot >/ql/log/bot/bot.log 2>&1 & fi "
+           "ps -ef | grep 'python3 -m jbot' | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null; " \
+           "nohup python3 -m jbot >/ql/log/bot/bot.log 2>&1 & fi "
     os.system(text)
-    
-    
+
+
 def start():
     file = "/jd/config/botset.json" if V4 else "/ql/config/botset.json"
-    with open(path, "r", encoding="utf-8") as f1:
+    with open(file, "r", encoding="utf-8") as f1:
         botset = f1.read()
     botset = botset.replace('user": "False"', 'user": "True"')
-    with open(path, "w", encoding="utf-8") as f2:
-        f2.write(botset)
-    
-    
-def close():
-    file = "/jd/config/botset.json" if V4 else "/ql/config/botset.json"
-    with open(path, "r", encoding="utf-8") as f1:
-        botset = f1.read()
-    botset = botset.replace('user": "True"', 'user": "False"')
-    with open(path, "w", encoding="utf-8") as f2:
+    with open(file, "w", encoding="utf-8") as f2:
         f2.write(botset)
 
+
+def close():
+    file = "/jd/config/botset.json" if V4 else "/ql/config/botset.json"
+    with open(file, "r", encoding="utf-8") as f1:
+        botset = f1.read()
+    botset = botset.replace('user": "True"', 'user": "False"')
+    with open(file, "w", encoding="utf-8") as f2:
+        f2.write(botset)
+
+
+def state():
+    file = "/jd/config/botset.json" if V4 else "/ql/config/botset.json"
+    with open(file, "r", encoding="utf-8") as f1:
+        botset = f1.read()
+    if 'user": "True"' in botset:
+        return True
+    else:
+        return False
+    
 
 @jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^/user$'))
 async def user_login(event):
@@ -73,28 +82,31 @@ async def user_login(event):
         login = False
         sender = event.sender_id
         session = "/jd/config/user.session" if V4 else "/ql/config/user.session"
-        if exists(session):
-            async with jdbot.conversation(sender, timeout=120) as conv:
-                msg = await conv.send_message("你已经拥有了一份user.session，请做出你的选择：")
-                buttons = [
-                    Button.inline("关闭user", data="close"),
-                    Button.inline("重新登录", data="relogin"),
-                    Button.inline('取消会话', data='cancel')
-                ]
-                msg = await jdbot.edit_message(msg, '请做出您的选择：', buttons=split_list(buttons, row))
-                convdata = await conv.wait_event(press_event(sender))
-                res = bytes.decode(convdata.data)
-                if res == 'cancel':
-                    await jdbot.edit_message(msg, '对话已取消')
-                    return
-                elif res == 'close':
-                    close()
-                    os.remove(session)
-                    restart()
-                else:
-                    login = True
-        else:
-            login = True
+        button = Button.inline("关闭user", data="close") if state() else Button.inline("开启user", data="start")
+        async with jdbot.conversation(sender, timeout=120) as conv:
+            msg = await conv.send_message("你已经拥有了一份user.session，请做出你的选择")
+            buttons = [
+                button,
+                Button.inline("重新登录", data="relogin"),
+                Button.inline('取消会话', data='cancel')
+            ]
+            msg = await jdbot.edit_message(msg, '你已经拥有了一份user.session，请做出你的选择：', buttons=split_list(buttons, row))
+            convdata = await conv.wait_event(press_event(sender))
+            res = bytes.decode(convdata.data)
+            if res == 'cancel':
+                await jdbot.edit_message(msg, '对话已取消')
+                return
+            elif res == 'close':
+                await jdbot.edit_message(msg, "关闭成功，准备重启机器人！")
+                close()
+                restart()
+            elif res == 'start':
+                await jdbot.edit_message(msg, "开启成功，请确保session可用，现准备重启机器人！")
+                start()
+                restart()
+            else:
+                await jdbot.edit_message(msg, "准备登陆程序！")
+                login = True
         if login:
             await user.connect()
             async with jdbot.conversation(sender, timeout=100) as conv:
@@ -104,7 +116,7 @@ async def user_login(event):
                 msg = await conv.send_message('请输入手机验证码:\n例如`12345`')
                 code = await conv.get_response()
                 await user.sign_in(phone.raw_text, code.raw_text.replace('code', ''))
-            await jdbot.send_message(chat_id, '恭喜您已登录成功！\n自动重启中！')
+                await jdbot.send_message(chat_id, '恭喜您已登录成功！\n自动重启中！')
             start()
             restart()
     except asyncio.exceptions.TimeoutError:
