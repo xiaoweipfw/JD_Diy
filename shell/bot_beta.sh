892464129b26641e4326ef8b152d6093fb70cbfa
chiupam <chiupam@126.com>
2022-01-03 15:57:50 +0800

    更新部署shell文件

--- shell/bot_beta.sh
+++ shell/bot_beta.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [ ! -d "/ql" ];then
-  dir_root=/jd
+  root=/jd
 else
-  dir_root=/ql
+  root=/ql
 fi
 
-dir_jbot=$dir_root/jbot
-dir_repo=$dir_root/repo
-file_bot=$dir_root/config/bot.json
-file_botset=$dir_root/config/botset.json
-file_diybotset=$dir_root/config/diybotset.json
+dir_jbot=$root/jbot
+dir_repo=$root/repo
+file_bot=$root/config/bot.json
+file_botset=$root/config/botset.json
+file_diybotset=$root/config/diybotset.json
 url="https://github.com/chiupam/JD_Diy.git"
 repo_path=$dir_repo/diybot
 
@@ -72,40 +72,42 @@ env() {
 bot() {
   echo -e "2、下载bot所需文件...\n"
   if [ -d ${repo_path}/.git ]; then
-      jbot_md5sum_old=$(cd $dir_jbot; find . -type f \( -name "*.py" -o -name "*.ttf" \) | xargs md5sum)
-      git_pull ${repo_path}
-      cp -rf $repo_path/beta/* $dir_jbot
-      jbot_md5sum_new=$(cd $dir_jbot; find . -type f \( -name "*.py" -o -name "*.ttf" \) | xargs md5sum)
-      if [[ "$jbot_md5sum_new" != "$jbot_md5sum_old" ]]; then
-          notify "检测到BOT程序有更新，BOT将重启。"
-      fi
+    jbot_md5sum_old=$(cd $dir_jbot; find . -type f \( -name "*.py" -o -name "*.ttf" \) | xargs md5sum)
+    git_pull ${repo_path}
+    cp -rf $repo_path/beta/* $dir_jbot
+    jbot_md5sum_new=$(cd $dir_jbot; find . -type f \( -name "*.py" -o -name "*.ttf" \) | xargs md5sum)
+    if [[ "$jbot_md5sum_new" != "$jbot_md5sum_old" ]]; then
+      notify "检测到BOT程序有更新，BOT将重启。\n为安全起见，关闭user监控，请使用 /user 手动开启！"
+    fi
   else
     git_clone ${url} ${repo_path} "main"
-      cp -rf $repo_path/beta/* $dir_jbot
+    cp -rf $repo_path/beta/* $dir_jbot
   fi
   echo -e "\nbot文件下载成功...\n"
 }
 
 init() {
   if [[ ! -f $file_bot ]]; then
-    cp -f $repo_path/config/bot.json $dir_root/config
+    cp -f $repo_path/config/bot.json $root/config
   fi
   if [[ ! -f $file_botset ]]; then
-    cp -f $repo_path/config/botset.json $dir_root/config
+    cp -f $repo_path/config/botset.json $root/config
   else
     sed -i 's/user": "True"/user": "False"/' $file_botset
+    echo "为安全起见，关闭user监控，请使用 /user 手动开启！"
   fi
   if [[ ! -f $file_diybotset ]]; then
-    cp -f $repo_path/config/diybotset.json $dir_root/config
+    cp -f $repo_path/config/diybotset.json $root/config
   else
     sed -i 's/key_1/listenerId/' $file_diybotset
-    sed -i 's/value_1/-100123456789/' $file_diybotset
+    sed -i 's/value_1/-1001630980165/' $file_diybotset
   fi
 }
 
 env_bot() {
   echo -e "3、安装python3依赖...\n"
   cd $dir_jbot
+  /usr/bin/python3 -m pip install --upgrade pip
   pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
   pip3 --default-timeout=100 install -r requirements.txt --no-cache-dir
   echo -e "\npython3依赖安装成功...\n"
@@ -113,23 +115,21 @@ env_bot() {
 
 start() {
   echo -e "4、启动bot程序...\n"
-  cd $dir_root
+  cd $root
   if [ ! -d $root/log/bot ]; then
-    mkdir $dir_root/log/bot
+    mkdir $root/log/bot
   fi
-  if [[ -z $(grep -E "123456789" $dir_root/config/bot.json) ]]; then
+  if [[ -z $(grep -E "123456789" $root/config/bot.json) ]]; then
     if [ -d "/ql" ]; then
       ps -ef | grep "python3 -m jbot" | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null
-      nohup python3 -m jbot >$dir_root/log/bot/bot.log 2>&1 &
+      nohup python3 -m jbot > $root/log/bot/bot.log 2>&1 &
     else
-      cd $dir_jbot
-      pm2 start ecosystem.config.js
-      cd $dir_root
-      pm2 restart jbot
+      cd $root/jbot || exit; pm2 start ecosystem.config.js
+      cd $root || exit; pm2 restart jbot
     fi
     echo -e "bot启动成功...\n"
   else
-    echo -e "配置 $dir_root/config/bot.json 后再次运行本程序即可启动机器人"
+    echo -e "配置 $root/config/bot.json 后再次运行本程序即可启动机器人"
   fi
 }
 
